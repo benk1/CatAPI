eafd2c7d8dcc27d0080a72b1a26dc286115d5300
benk1 <kakengi_bernard@hotmail.com>
2019-12-13 09:27:43 +0200

    Updates

---
+++ precache-manifest.e1fc3d1161c56f7dbda7ccb16333cfb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2469bf6e30ed674ecc21cc949e025689",
+    "url": "/CatAPI/index.html"
+  },
+  {
+    "revision": "a4ad41a503f63dec85c7",
+    "url": "/CatAPI/static/css/main.04f96ee5.chunk.css"
+  },
+  {
+    "revision": "ba54248c709b46e66783",
+    "url": "/CatAPI/static/js/2.78faed18.chunk.js"
+  },
+  {
+    "revision": "a4ad41a503f63dec85c7",
+    "url": "/CatAPI/static/js/main.b4e25170.chunk.js"
+  },
+  {
+    "revision": "12471592ef3c32d90d25",
+    "url": "/CatAPI/static/js/runtime-main.130c4ff4.js"
+  }
+]);
