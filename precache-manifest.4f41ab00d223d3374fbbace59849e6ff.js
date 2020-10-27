16911bdc60a35eef5065fa1c336e4b27c33c1c20
benk1 <kakengi_bernard@hotmail.com>
2020-10-27 22:05:40 +0200

    Updates

--- precache-manifest.4f41ab00d223d3374fbbace59849e6ff.js
+++ precache-manifest.4f41ab00d223d3374fbbace59849e6ff.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b0304f29a2b3580bedb9c7ef42f7f4cf",
+    "revision": "d81c9a7002fcf0e7f80171d3b2c0f8fc",
     "url": "/CatAPI/index.html"
   },
   {
-    "revision": "be5841d385416042cf89",
-    "url": "/CatAPI/static/css/main.fb9d57da.chunk.css"
+    "revision": "79454331ff62bcf9d10d",
+    "url": "/CatAPI/static/css/main.66a62343.chunk.css"
   },
   {
     "revision": "ba54248c709b46e66783",
     "url": "/CatAPI/static/js/2.78faed18.chunk.js"
   },
   {
-    "revision": "be5841d385416042cf89",
-    "url": "/CatAPI/static/js/main.7520411f.chunk.js"
+    "revision": "79454331ff62bcf9d10d",
+    "url": "/CatAPI/static/js/main.eb8635ed.chunk.js"
   },
   {
     "revision": "12471592ef3c32d90d25",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CatAPI/precache-manifest.dac9542d01abbd19c7db8b77bab525cf.js"
+  "/CatAPI/precache-manifest.4f41ab00d223d3374fbbace59849e6ff.js"
 );
 
 self.addEventListener('message', (event) => {
